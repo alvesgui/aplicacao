c059a8cd74c8d7a99961b794360fe56e97b68f2d
Guilherme <guylherme.alves14@gmail.com>
2020-06-10 14:54:12 -0300

    Alterações finais

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController{
             return { 
                 id: item.id,
                 title: item.title,
-                image_url: `http://192.168.0.116:3333/uploads/${item.image}`,
+                image_url: `http://192.168.43.55:3333/uploads/${item.image}`,
             };
         })
     
